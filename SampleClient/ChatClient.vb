496aa966342d22320d56954f47d8435caa6b8a82
nikeee13 <nikeee@outlook.com>
2013-04-12 11:14:52 +0200

    still not done

--- SampleClient/ChatClient.vb
+++ SampleClient/ChatClient.vb
@@ -7,6 +7,8 @@ Class ChatClient
     Private _name As String
     Private _ep As IPEndPoint
 
+    Private _ns As NetworkStream
+
     Sub New(name As String, endPoint As IPEndPoint)
         If String.IsNullOrWhiteSpace(name) Then Throw New ArgumentNullException("name")
         If endPoint Is Nothing Then Throw New ArgumentNullException("endPoint")
@@ -18,10 +20,17 @@ Class ChatClient
 
     Sub Connect()
         _c.Connect(_ep)
+        _ns = _c.GetStream()
     End Sub
 
-    Public Sub SendMessage()
-        'TODO: Send a message
+    Public Sub SendMessage(message As String)
+        SendPacket(New SendMessagePacket(_name, message))
+    End Sub
+
+    Private Sub SendPacket(p As IChatPacket)
+        If Not _c.Client.Connected OrElse _ns Is Nothing Then Throw New InvalidOperationException()
+        'TODO: Send packet
+
     End Sub
 
 End Class
---
+++ SampleClient/ChatPackets/ChackPacketReader.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO
+
+Class ChackPacketHandler
+    Public Function GetPacketData(p As SendMessagePacket) As Byte()
+        Using ms As New MemoryStream()
+            Using wtr As New BinaryWriter(ms)
+                wtr.Write(p.From)
+                wtr.Write(p.Message)
+                wtr.Flush()
+            End Using
+            Return ms.ToArray()
+        End Using
+    End Function
+    Public Function GetPacketData(bytes As Byte()) As GotMessagePacket
+        Using ms As New MemoryStream(bytes)
+            Using rdr As New BinaryReader(ms)
+                Dim from = rdr.ReadString()
+                Dim msg = rdr.ReadString()
+                Return New GotMessagePacket(from, msg)
+            End Using
+        End Using
+    End Function
+End Class
+
+Interface IChatPacket
+End Interface
+
+Structure GotMessagePacket
+    Implements IChatPacket
+
+    Public From As String
+    Public Message As String
+    Sub New(frm As String, msg As String)
+        From = frm
+        Message = msg
+    End Sub
+End Structure
+
+Structure SendMessagePacket
+    Implements IChatPacket
+
+    Public From As String
+    Public Message As String
+    Sub New(frm As String, msg As String)
+        From = frm
+        Message = msg
+    End Sub
+End Structure
